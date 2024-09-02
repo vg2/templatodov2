5fb01fffbccc68fe6fdd37e87449a87cae92843b
Vishen Gounden <vishen.gounden@glacier.co.za>
2024-09-02 16:35:11 +0200

    update formatting

--- app/src/service/insert-todo.ts
+++ app/src/service/insert-todo.ts
@@ -3,10 +3,9 @@ import { openDb } from "../data/db";
 import type { DbTodoItem } from "@app/data/DbTodoItem.type";
 
 export async function insertTodo(todo: TodoItem): Promise<void> {
-    const db = await openDb();
-    const dbTodo: DbTodoItem = {
-        ...todo
-    }
-    await db.put('todoItems', dbTodo);
+  const db = await openDb();
+  const dbTodo: DbTodoItem = {
+    ...todo,
+  };
+  await db.put("todoItems", dbTodo);
 }
-
