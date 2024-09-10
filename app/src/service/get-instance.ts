44eca5f0fabf88b27803d42c85a25b69e040e2a1
Vishen Gounden <vishen.gounden@glacier.co.za>
2024-09-10 16:45:37 +0200

    start improving get instance behaviour

--- app/src/service/get-instance.ts
+++ app/src/service/get-instance.ts
@@ -1,20 +1,26 @@
+import { isAfter, parseISO } from "date-fns";
 import type { DbTemplateInstance } from "../data/DbTemplateInstance.type";
 import { openDb } from "../data/db";
 import { type TemplateInstance, mapTemplateInstanceFromDb } from "../model/TemplateInstance.type";
 import { loadTemplate } from "./load-templates";
 
+const utcMidnight = 'T00:00:00Z';
+
 export async function getInstance(templateId: number | null, date: string): Promise<TemplateInstance | null> {
     if (!templateId) return Promise.resolve(null);
     
     const db = await openDb();
-    const dbTemplateInstances = await db.getAll('templateInstances');
+    const allDbTemplateInstances = await db.getAll('templateInstances');
+    const dbTemplateInstances = allDbTemplateInstances.filter(adbi => adbi.templateSnapshot.id === templateId);
     const template = await loadTemplate(templateId);
     const templateInstances = dbTemplateInstances.map(dbti => mapTemplateInstanceFromDb(dbti, template));
-    // todo: use better logic to determine if existing instance is applicable
-    // when it can span days
-    const templateInstance = templateInstances.find(ti => ti.templateSnapshot.id === templateId && ti.date === date); 
+    const dateOrderableTemplateInstances = templateInstances.map(ti => ({ instance: ti, date: parseISO(`${ti.date}${utcMidnight}`)}));
+    const orderedTemplateInstances = dateOrderableTemplateInstances.sort((a,b) => isAfter(b.date, a.date) ? 1 : -1);
+
+    // todo: check if date param is valid for latest (first in ordered list) template instance
+    const templateInstance = orderedTemplateInstances.find(ti => ti.instance.date === date); 
     if (templateInstance) {
-        return templateInstance;
+        return templateInstance.instance;
     }
 
     const dbTemplate = await db.get('templates', templateId);
